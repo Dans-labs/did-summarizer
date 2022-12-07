ec658dbfb95160851e3bc0d6775c1af0027b9be8
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2022-12-07 21:33:14 +0100

    Simple tests added

--- distros/DID/tests/simple-pytest.py
+++ distros/DID/tests/simple-pytest.py
@@ -3,6 +3,7 @@ import json
   
 # defining the api-endpoint 
 API_ENDPOINT = "http://0.0.0.0:8000/cache"
+API_ENDPOINT = "https://api.zandbak.dans.knaw.nl/cache"
   
 # your API key here
 API_KEY = "XXXXXXXXXXXXXXXXX"
@@ -12,5 +13,11 @@ urls = ['url_4','url_5','url_6']
 data = {}
 data['url'] = urls
 print(data)
+r = requests.post(url = API_ENDPOINT, data = json.dumps(data))
+print(r.text)
+
+data = {}
+persons = ["Rutte", "Zelenskyy", "Biden"]
+data['url'] = persons
 r = requests.post(url = API_ENDPOINT, data = json.dumps(data))
 print(r.text)
